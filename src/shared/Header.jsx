a19a8a8bbdac824e3963cd4005510ec3195e1a4f
Fedotov Aleksey <far@rio-soft.ru>
2020-05-05 16:38:00 +0500

    Настройка ssr

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <header>
-            <h1>Reddit for our own</h1>
+            <h1>Reddit for our own!!!</h1>
         </header>
     )
 }
