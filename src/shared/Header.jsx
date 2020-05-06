288b106249b83b617334bf79312e741788e200c4
Fedotov Aleksey <far@rio-soft.ru>
2020-05-06 11:18:09 +0500

    Подключение Hot module replacement к Server side rendering приложению

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,12 @@
+import {hot} from 'react-hot-loader/root';
 import React from 'react';
 
-export const Header = () => {
+const HeaderComponent = () => {
     return (
         <header>
             <h1>Reddit for our own!!!</h1>
         </header>
     )
 }
+
+export const Header = hot(HeaderComponent)
